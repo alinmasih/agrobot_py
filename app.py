e5a18a674f55b6fe88b97e85b6842ae5bef393b2
alinmasih <hialinmasih@gmail.com>
2026-04-21 06:46:16 +0530

    Change default port from 10000 to 5000

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def predict():
 
 if __name__ == "__main__":
     # Render uses the PORT environment variable
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
